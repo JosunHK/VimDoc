b9a104b38192309479b4791f5c3b6d4b0218740d
JosunHK <dennistai.mail@gmail.com>
2022-05-22 19:51:46 +0800

    added some movement commands

--- src/canvas/canvas.jsx
+++ src/canvas/canvas.jsx
@@ -5,25 +5,90 @@ import Line from './line/line'
 
 const[key, setKey] = createSignal("");
 const[lines, setLines] = createSignal([
-  {index: 1, content: ""},
+  {content: ""},
 ]);
 
+//TODO: moves to another class
+//commands
+function moveCaret(window, fromEle, toEle){
+  //only toEle is needed now, other params might not be needed;
+  toEle.focus();
+}
+
+function newLineEvent(event){
+  event.preventDefault();
+  setLines([...lines(), {content : ""}]);
+  const fromEle = event.target;
+  const toEle = document.getElementById(fromEle.id * 1 + 1);
+  moveCaret(window, fromEle, toEle);
+}
+
+function lineUpEvent(event){
+  event.preventDefault();
+  const fromEle = event.target;
+  let toId = Math.max(fromEle.id * 1 - 1, 1); //can't go above first line 
+  const toEle = document.getElementById(toId);
+  moveCaret(window, fromEle, toEle);
+}
+
+function lineDownEvent(event){
+  event.preventDefault();
+  const fromEle = event.target;
+  let toId = Math.min(fromEle.id * 1 + 1, lines().length); //can't go below last line 
+  const toEle = document.getElementById(toId);
+  moveCaret(window, fromEle, toEle);
+}
+
+function indentEvent(event){
+  event.preventDefault();
+  let id = event.target.id * 1 - 1;
+  let line = lines()[id];
+  let lineContent = line.content;
+  line.content = "    " + line.content;
+  lines()[id] = line;
+  console.log(lines()[id]);
+  setLines(lines());
+}
+//commands
 export default function Canvas() {
-
-
   return (
     <>
     <div class="canvas">
       <For each={lines()}>{(line, i) =>
-        <Line index={line.index} content={line.content}/>
+        <Line index={i() + 1} content={line.content}/>
       }</For>
     </div>
     </>
   );
 }
 
+export function handleChange(event){
+  console.log("change");
+  let content = event.target.innerHTML;
+  let id = event.target.id;
+  let line = lines()[id];
+  line.content = content;
+  lines()[id] = line;
+  console.log(lines()[id]);
+  setLines(lines());
+}
+
 export function handleKeypress(event) {
-  event.preventDefault();
-  setKey(event.code);
-  setLines([...lines(), {index: lines().length + 1, content : ""}]);
+  if(event.code === 'Enter'){
+    newLineEvent(event);
+  }
+}
+
+export function handleKeydown(event) {
+  if(event.code === 'ArrowUp'){
+    lineUpEvent(event);
+  }
+
+  if(event.code === 'ArrowDown'){
+    lineDownEvent(event);
+  }
+
+  if(event.code === 'Tab'){
+    indentEvent(event);
+  }
 }
--- src/canvas/line/line.jsx
+++ src/canvas/line/line.jsx
@@ -1,12 +1,19 @@
 import "./line.css";
 import { createSignal, Index } from 'solid-js';
-import { handleKeypress } from './../canvas'
+import { handleKeypress, handleKeydown, handleChange} from './../canvas'
 
 export default function Line(props) {
+
   return (
     <><div class="line">
       <span class="index">{props.index}</span>
-      <span class="content" onKeypress={handleKeypress} contenteditable="true">{props.content}</span>
+      <span class="content" id={props.index}
+        onKeypress={handleKeypress}
+        onKeydown={handleKeydown}
+        onChange={handleChange}
+        contenteditable="true">
+        {props.content}
+      </span>
     </div></>
   );
 }
