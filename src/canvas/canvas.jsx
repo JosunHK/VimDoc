607daf97b6f115430de94448879ea83691e0bc06
Josun <dennistai.mail@gmail.com>
2022-09-05 10:56:16 +0800

    bruh

--- src/canvas/canvas.jsx
+++ src/canvas/canvas.jsx
@@ -3,27 +3,39 @@ import { createSignal, Index } from 'solid-js';
 import "./canvas.css";
 import Line from './line/line'
 
-const[key, setKey] = createSignal("");
-const[lines, setLines] = createSignal([
-  {content: <><span style='color:red'>test</span><span style='color:blue'>test</span></>},
+const NORMAL_MODE = 0;
+const INSERT_MODE = 1;
+const VISUAL_MODE = 2;
+
+const [key, setKey] = createSignal("");
+const [lines, setLines] = createSignal([
+  {
+    content: 
+    <>
+      <span style='color:red'>test&nbsp;</span>
+      <span style='color:blue'>test</span>
+    </>
+  },
 ]);
+
+let mode = 0;
 
 //TODO: moves to another class
 //commands
-function moveCaret(window, fromEle, toEle){
+function moveCaret(window, fromEle, toEle) {
   //only toEle is needed now, other params might not be needed;
   toEle.focus();
 }
 
-function newLineEvent(event){
+function newLineEvent(event) {
   event.preventDefault();
-  setLines([...lines(), {content : ""}]);
+  setLines([...lines(), { content: "" }]);
   const fromEle = event.target;
   const toEle = document.getElementById(fromEle.id * 1 + 1);
   moveCaret(window, fromEle, toEle);
 }
 
-function lineUpEvent(event){
+function lineUpEvent(event) {
   event.preventDefault();
   const fromEle = event.target;
   let toId = Math.max(fromEle.id * 1 - 1, 1); //can't go above first line 
@@ -31,7 +43,18 @@ function lineUpEvent(event){
   moveCaret(window, fromEle, toEle);
 }
 
-function lineDownEvent(event){
+function moveCaretRight(event) {
+  event.preventDefault();
+  let ele = event.target;
+  if (typeof ele.createTextRange != "undefined") {
+        ele.focus();
+        var range = ele.createTextRange();
+        range.collapse(false);
+        range.select();
+  }
+}
+
+function lineDownEvent(event) {
   event.preventDefault();
   const fromEle = event.target;
   let toId = Math.min(fromEle.id * 1 + 1, lines().length); //can't go below last line 
@@ -39,11 +62,9 @@ function lineDownEvent(event){
   moveCaret(window, fromEle, toEle);
 }
 
-function indentEvent(event){
+function indentEvent(event) {
   event.preventDefault();
-
   console.log(event.target);
-
   //init
   let indent = <span class="tab">&nbsp;&nbsp;&nbsp;&nbsp;</span>;
   console.log(indent);
@@ -55,6 +76,9 @@ function indentEvent(event){
   let modifiedString = "";
 
   //modify the innerHTML to add tabspace after the cursor, different handling for end of line;
+  console.log("content");
+  console.log(content);
+  console.log(selection);
 
 }
 //commands
@@ -62,18 +86,18 @@ function indentEvent(event){
 export default function Canvas() {
   return (
     <>
-    <div class="canvas">
-      <For each={lines()}>{(line, i) =>
-        <Line index={i() + 1} content={line.content}/>
-      }</For>
-    </div>
+      <div class="canvas">
+        <For each={lines()}>{(line, i) =>
+          <Line index={i() + 1} content={line.content} />
+        }</For>
+      </div>
     </>
   );
 }
 
-export function updateContent(event){
+export function updateContent(event) {
   let content = event.target.innerHTML;
-  let id = event.target.id *1 - 1;
+  let id = event.target.id * 1 - 1;
   let line = lines()[id];
   line.content = content;
   lines()[id] = line;
@@ -82,23 +106,38 @@ export function updateContent(event){
 
 export function handleKeypress(event) {
   console.log(event.code)
-  if(event.code === 'Enter'){
+  if(mode !== INSERT_MODE)
+    event.preventDefault();
+
+  if (event.code === 'Enter') {
     newLineEvent(event);
   }
 }
 
 export function handleKeydown(event) {
   console.log(event.code)
-  if(event.code === 'ArrowUp'){
+  if (event.code === 'ArrowUp' || event.code === 'KeyK') {
     lineUpEvent(event);
   }
 
-  if(event.code === 'ArrowDown'){
+  if (event.code === 'ArrowDown' || event.code === 'KeyJ') {
     lineDownEvent(event);
   }
 
-  if(event.code === 'Tab'){
+  if (event.code === 'Tab') {
     indentEvent(event);
+  }
+
+  if (event.code === 'Escape') {
+    mode = NORMAL_MODE;
+  }
+
+  if (event.code === 'KeyI') {
+    mode = INSERT_MODE;
+  }
+
+  if(event.code === 'KeyL'){
+    moveCaretRight(event);
   }
 }
 
